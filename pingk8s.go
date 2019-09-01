91a2444311395a2ec9d3efdca6d6b589f952191e
Alexandru Bonini <alex@alexandrus-mbp.localdomain>
2019-09-01 23:51:49 +0200

    add prometheus exporter
    Signed-off-by: Alexandru Bonini <alex@alexandrus-mbp.localdomain>

--- pingk8s.go
+++ pingk8s.go
@@ -18,14 +18,39 @@ limitations under the License.
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sparrc/go-ping"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+var (
+	addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+)
+
+var (
+	pingDurations = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "ping_durations_seconds",
+			Help:       "ping latency distributions.",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		},
+		[]string{"service"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(pingDurations)
+	prometheus.MustRegister(prometheus.NewBuildInfoCollector())
+}
 
 func main() {
 	// creates the in-cluster config
@@ -39,19 +64,22 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		for _, pod := range pods.Items {
-			if !pod.Spec.HostNetwork {
-				go pinger(pod.GetName(), pod.Status.PodIP)
-			}
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	for _, pod := range pods.Items {
+		if !pod.Spec.HostNetwork {
+			go pinger(pod.GetName(), pod.Status.PodIP)
 		}
 	}
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
 func pinger(name, ip string) {
@@ -65,6 +93,11 @@ func pinger(name, ip string) {
 		pinger.Run()
 		stats := pinger.Statistics()
 		fmt.Printf("%s %s %v\n", name, ip, stats)
+		pingDurations.WithLabelValues(fmt.Sprintf("%v %v MinRtt", name, ip)).Observe(float64(stats.MinRtt))
+		pingDurations.WithLabelValues(fmt.Sprintf("%v %v MaxRtt", name, ip)).Observe(float64(stats.MaxRtt))
+		pingDurations.WithLabelValues(fmt.Sprintf("%v %v PacketsSent", name, ip)).Observe(float64(stats.PacketsSent))
+		pingDurations.WithLabelValues(fmt.Sprintf("%v %v PacketsRecv", name, ip)).Observe(float64(stats.PacketsRecv))
+		pingDurations.WithLabelValues(fmt.Sprintf("%v %v PacketLoss", name, ip)).Observe(float64(stats.PacketLoss))
 		time.Sleep(3 * time.Second)
 	}
 }
